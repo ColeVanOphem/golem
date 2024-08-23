373b6926612838eeeee52497ed8c6e49d38eacb1
Cole VanOphem <nvano@umich.edu>
2024-08-23 01:49:16 -0400

    Add main

--- main.go
+++ main.go
@@ -1,26 +1,73 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/anthdm/foreverstore/p2p"
 )
 
-func main() {
-
-	tcpOpts := p2p.TCPTransportOptions{
-		ListenAddr:    ":3000",
+func makeServer(listenAddr string, nodes ...string) *FileServer {
+	tcpTransportOpts := p2p.TCPTransportOptions{
+		ListenAddr:    listenAddr,
 		HandshakeFunc: p2p.NOPHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
 	}
-	tr := p2p.NewTCPTransport(tcpOpts)
+	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 
-	if err := tr.ListenAndAccept(); err != nil {
-		log.Fatal(err)
+	fileServerOpts := FileServerOpts{
+		EncKey:            newEncryptionKey(),
+		StorageRoot:       listenAddr + "_network",
+		PathTransformFunc: DefaultPathTransformFunc,
+		Transport:         tcpTransport,
+		BootstrapNodes:    nodes,
 	}
 
-	select {}
+	s := NewFileServer(fileServerOpts)
 
-	fmt.Println("Hello World!")
+	tcpTransport.OnPeer = s.OnPeer
+
+	return s
+}
+
+func main() {
+	s1 := NewFileServer(":3000", "")
+	s2 := NewFileServer(":5000", "")
+	s3 := NewFileServer(":7000", ":3000", ":5000")
+
+	go func() { log.Fatal(s1.Start()) }()
+	time.Sleep(2 * time.Second)
+
+	go func() { log.Fatal(s2.Start()) }()
+	time.Sleep(2 * time.Second)
+
+	go s3.Start()
+	time.Sleep(2 * time.Second)
+
+	for i := 0; i < 20; i++ {
+		key := fmt.Sprintf("picture_%d.png", i)
+		data := bytes.NewReader([]byte("my big cat picture"))
+
+		s3.Store(key, data)
+
+		if err := s3.store.Delete(s3.ID, key); err != nil {
+			log.Fatal(err)
+		}
+
+		r, err := s3.Get(key)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(b))
+
+	}
 }
